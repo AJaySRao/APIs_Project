2b7c8f5aa0266362070e148a90c51da32dd42ef5
changeAJay <ajajay008@gmail.com>
2022-03-19 09:53:27 +0530

    Checks if the iss is in local position

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import requests
-LAT = 16.466510
-LNG = 81.478336
+from datetime import datetime, timezone
+import smtplib
+import time
 
-parameters = {
-    "lat": LAT,
-    "lng": LNG
-}
+LAT = 16.544893
+LNG = 81.521240
+EMAIL = "samplgemail9@gmail.com"
+PASS = "9samplegmail"
 
-res = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-res.raise_for_status()
-data = res.json()
-print(data)
+
+def iss_in_local_position():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_lat = float(data["iss_position"]["latitude"])
+    iss_lng = float(data["iss_position"]["longitude"])
+    if LAT-5 <= iss_lat <= LAT+5 and LNG-5 <= iss_lng <= LNG+5:
+        return True
+
+
+
+
+
